3fafd502ee9bf94f9fad4b61ea77bdd72de69641
senamit2708 <senamit2708@gmail.com>
2017-11-28 22:05:22 +0530

    Coordinate Layout working with snackbar

--- app/src/main/java/com/example/senamit/collectionofdata/MainActivity.java
+++ app/src/main/java/com/example/senamit/collectionofdata/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.senamit.collectionofdata;
 
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSnackbar = (Button)findViewById(R.id.btn_snackbar);
+        final CoordinatorLayout coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinator_main);
+        btnSnackbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Snackbar snackbar = Snackbar.make(coordinatorLayout, "simple snackbar", Snackbar.LENGTH_LONG);
+                snackbar.show();
+
+            }
+        });
+
+
     }
 }
